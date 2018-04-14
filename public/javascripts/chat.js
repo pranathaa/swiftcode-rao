3e21941c7edbea61cefd1991e7c58a7c4b4baa0c
pranathaa <pranatharao96@gamil.com>
2018-04-14 12:13:05 +0000

    3rd commit

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,25 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'hello'
+    },
+        {
+            'sender': 'BOT',
+            'text': 'Hi what can i do for u'
+    },
+        {
+            'sender': 'USER',
+            'text': 'Help me in my peoject'
+    },
+        {
+            'sender': 'BOT',
+            'text': 'What the topic'
+    },
+        {
+            'sender': 'USER',
+            'text': 'Its about IoT'
+    }
+    ];
 });
